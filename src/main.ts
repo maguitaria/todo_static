164df42065e59da4a64dc894c1854ba0f0b7f870
maguitaria <t2glma00@students.oamk.fi>
2023-02-10 13:47:56 +0200

    added main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './css/style.css'
+import '.src/css/style.css'
 import FullList from './model/FullList';
 import ListItem from './model/ListItem';
 import ListTemplate from './templates/ListTemplate';
